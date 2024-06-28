41725e2511422bca8f84ce8f32c798291454cc74
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-06-28 17:39:30 +0500

    Final commit

---
+++ Random Color Generator/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import RandomColorGenerator from "./components/RandomColorGenerator";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <RandomColorGenerator />
+    </>
+  );
+}
+
+export default App;
---
+++ Random Color Generator/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 bg-blue-800  text-white h-[70px] flex items-center justify-center">
+      <h1 className="font-bold text-2xl ">Random Color Generator</h1>
+    </header>
+  );
+};
+
+export default Header;
