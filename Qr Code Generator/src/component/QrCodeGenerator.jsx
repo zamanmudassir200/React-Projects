4958adf316cf6a42d20ab1bcf2ed4a661e3a7382
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-06-30 04:00:04 +0500

    Final commit

--- Qr Code Generator/src/component/QrCodeGenerator.jsx
+++ Qr Code Generator/src/component/QrCodeGenerator.jsx
@@ -12,9 +12,9 @@ const QrCodeGenerator = () => {
   return (
     <div className="flex max-w-[1000px] px-2 mx-auto  flex-col h-screen pt-10 items-center justify-start">
       <h1 className="text-white font-bold text-3xl py-4">Qrcode Generator</h1>
-      <div className="border-2 min-w-[70%] sm:min-w-[70%] overflow-hidden">
+      <div className=" flex flex-col sm:flex-row sm:border-2 border-2 w-[90%] sm:min-w-[70%] overflow-hidden">
         <input
-          className=" px-3 py-2  sm:min-w-[80%] outline-none bg-blue-500 text-white font-semibold placeholder:text-gray-300 "
+          className=" px-3 py-2 w-[100%]  sm:min-w-[80%] outline-none bg-blue-500 text-white font-semibold placeholder:text-gray-300 "
           type="text"
           onChange={(e) => setInput(e.target.value)}
           name="qr-code"
@@ -24,13 +24,17 @@ const QrCodeGenerator = () => {
         <button
           disabled={input && input.trim() !== "" ? false : true}
           onClick={handleGenerateQrCode}
-          className="bg-white px-4 py-2 min-w-[10%] sm:w-[20%] text-blue-600 font-bold "
+          className="bg-white px-4 py-2 sm:min-w-[10%] sm:w-[20%] text-blue-600 font-bold "
         >
           Generate
         </button>
       </div>
-      <div className="mt-7 ">
-        <QRCode className="min-w-[390px]" value={qrCode} bgColor="white" />
+      <div className="mt-7 flex items-center justify-center">
+        <QRCode
+          className="sm:w-[100%] w-[90%]"
+          value={qrCode}
+          bgColor="white"
+        />
       </div>
     </div>
   );
