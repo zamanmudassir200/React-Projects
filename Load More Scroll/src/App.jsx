e07db8701a8ffc50f43ae535eec21bb0609f8b8e
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-06-29 22:09:32 +0500

    Add initial files

---
+++ Load More Scroll/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import LoadMore from "./component/LoadMore";
+
+function App() {
+  return (
+    <>
+      <LoadMore />
+    </>
+  );
+}
+
+export default App;
---
+++ Load More Scroll/src/component/LoadMore.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoadMore = () => {
+  return <div>loadmore</div>;
+};
+
+export default LoadMore;
