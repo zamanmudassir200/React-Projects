dae16d850f236489af844e6b83671c539ccde318
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-07-02 03:03:59 +0500

    final commit

--- Custom Tabs/src/components/Tabs.jsx
+++ Custom Tabs/src/components/Tabs.jsx
@@ -7,22 +7,32 @@ const Tabs = ({ tabs, onChange }) => {
     onChange(getCurrentIndex);
   };
   return (
-    <div className="h-screen py-5">
+    <div className="h-screen py-5 bg-purple-500">
       <div className="flex items-center justify-center">
         {tabs.map((tabItem, index) => {
           return (
             <div
               onClick={() => handleOnClick(index)}
-              className="bg-red-300 p-4 cursor-pointer border-2  border-transparent hover:border-2 hover:border-red-800"
+              className={`${
+                index === currentTabIndex ? "bg-green-800 border-b-4 " : ""
+              } bg-orange-300 p-4  cursor-pointer `}
               key={tabItem.label}
             >
-              <span>{tabItem.label}</span>
+              <span
+                className={`${
+                  index === currentTabIndex ? "text-white font-bold" : ""
+                }`}
+              >
+                {tabItem.label}
+              </span>
             </div>
           );
         })}
       </div>
-      <div className="max-w-[700px] mx-auto my-10">
-        {tabs[currentTabIndex] && tabs[currentTabIndex].content}
+      <div className=" bg-slate-500 p-6 h-[calc(100vh-92px)] my-3 ">
+        <div className="max-w-[700px] mx-auto text-lg border-2 rounded-lg p-2 text-white">
+          {tabs[currentTabIndex] && tabs[currentTabIndex].content}
+        </div>
       </div>
     </div>
   );
