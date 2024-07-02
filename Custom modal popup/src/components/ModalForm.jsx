52252893614b68ab0298c6d1849540faac060b13
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-07-03 01:14:05 +0500

    final commit

--- Custom modal popup/src/components/ModalForm.jsx
+++ Custom modal popup/src/components/ModalForm.jsx
@@ -11,7 +11,7 @@ const ModalForm = ({
     <div
       className={`${
         formModal ? "block absolute" : "hidden"
-      }  top-0 left-0 py-10 bg-red-500 h-[calc(100vh-70px)] w-[100vw] bg-opacity-25`}
+      }  top-0 left-0 py-10 px-4  bg-red-500 h-[calc(100vh-70px)] w-[100vw] bg-opacity-25`}
     >
       <div className="relative flex flex-col h-[600px] border-2 border-black max-w-[900px] items-center gap-8 bg-white rounded-md text-xl justify-center mx-auto">
         <h1 className="text-3xl font-bold">Your Details</h1>
