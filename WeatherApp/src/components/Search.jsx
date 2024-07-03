625797bf8e153ecc7c0a673192707e22110d3373
Muhammad Mudassir Zaman <127381696+zamanmudassir200@users.noreply.github.com>
2024-07-04 00:15:57 +0500

    pictures added

--- WeatherApp/src/components/Search.jsx
+++ WeatherApp/src/components/Search.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 
-const Search = ({ search, setSearch, handleSearch }) => {
+const Search = ({ search, setSearch, handleSearch, errMsg }) => {
   return (
-    <div className="flex p-4 m sm:flex-row flex-col w-[100%]  items-center justify-center">
-      <input
-        className="px-2 font-semibold w-[100%] sm:w-[80%] py-2  focus:outline-none"
-        type="text"
-        placeholder="Enter city"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <button
-        onClick={handleSearch}
-        className="bg-green-400 text-white font-semibold w-[100%] sm:w-[20%] px-2 py-2"
-      >
-        Search
-      </button>
-    </div>
+    <>
+      <div className="flex p-4 m sm:flex-row flex-col w-[100%]  items-center justify-center">
+        <input
+          className="px-2 font-semibold w-[100%] sm:w-[75%] py-2  focus:outline-none"
+          type="text"
+          placeholder="Enter city"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSearch();
+            }
+          }}
+        />
+        <button
+          onClick={handleSearch}
+          className="bg-green-400 text-white font-semibold w-[100%] sm:w-[20%] px-2 py-2"
+        >
+          Search
+        </button>
+      </div>
+    </>
   );
 };
 
--- WeatherApp/src/components/Weather.jsx
+++ WeatherApp/src/components/Weather.jsx
@@ -1,34 +1,169 @@
+// import React, { useEffect, useState } from "react";
+// import Search from "./Search";
+
+// const Weather = () => {
+//   const [search, setSearch] = useState("karachi");
+//   const [loading, setLoading] = useState(false);
+//   const [errMsg, setErrMsg] = useState(null);
+//   const [weatherData, setWeatherData] = useState("");
+
+//   const fetchWeatherData = async (param) => {
+//     if (search.trim() !== "") {
+//       try {
+//         setLoading(true);
+//         const response = await fetch(
+//           `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=5d6edc0e197ad2988ad739c90741f149&units=metric
+//         `
+//         );
+//         const data = await response.json();
+//         console.log(data);
+//         setWeatherData(data);
+//         setLoading(false);
+//       } catch (error) {
+//         console.log("error: ", error.message);
+//         setErrMsg(error.message);
+//         setLoading(false);
+//       }
+//     } else {
+//       alert("Please enter city...");
+//     }
+//   };
+//   useEffect(() => {
+//     fetchWeatherData("karachi");
+//   }, []);
+//   const handleSearch = async () => {
+//     fetchWeatherData(search);
+//   };
+
+//   if (loading) {
+//     return (
+//       <div className="text-xl flex items-center justify-center h-screen font-bold">
+//         <h1 className="text-center">Loading data! Please wait....</h1>
+//       </div>
+//     );
+//   }
+
+//   if (errMsg !== null) {
+//     return (
+//       <div className="text-xl flex items-center justify-center h-screen font-bold">
+//         <h1 className="text-center">
+//           Error Occured! <span className="text-red-600">{errMsg}</span>
+//         </h1>
+//       </div>
+//     );
+//   }
+//   const getCurrentDate = () => {
+//     return new Date().toLocaleDateString("en-us", {
+//       weekday: "long",
+//       month: "long",
+//       day: "numeric",
+//       year: "numeric",
+//     });
+//   };
+//   return (
+//     <div className="flex flex-col items-center  p-2 justify-center w-full min-h-screen">
+//       <div className="flex min-w-[90%] sm:min-w-[60%] rounded-2xl shadow-xl bg-blue-500 m-2 mx-auto border-2 min-h-[700px] pt-2 flex-col items-center justify-start">
+//         <h1 className="text-center font-extrabold text-white  text-3xl sm:text-4xl">
+//           Weather Forecast
+//         </h1>
+
+//         <Search
+//           search={search}
+//           setSearch={setSearch}
+//           handleSearch={handleSearch}
+//         />
+//         <div className="text-white font-extrabold flex  flex-col gap-5  text-center p-5 min-h-[500px] rounded-sm sm:w-[90%] ">
+//           <div className="bg-black text-white rounded-md">
+//             <h2 className="font-bold text-3xl py-2">
+//               {weatherData?.name}, {weatherData?.sys?.country}
+//             </h2>
+//           </div>
+//           <div className="">
+//             <span className="font-semibold text-xl py-1">
+//               {getCurrentDate()}
+//             </span>
+//           </div>
+//           <div className="font-semibold text-xl">
+//             Temperature: {weatherData?.main?.temp}&deg;C
+//           </div>
+//           <div className="flex flex-col items-center justify-center">
+//             <img
+//               className="w-[150px] h-[150px] "
+//               src={`../images/${
+//                 weatherData && weatherData.weather[0]
+//                   ? weatherData.weather[0].main
+//                   : null
+//               }.png`}
+//               alt=""
+//             />
+//             <p>
+//               {weatherData && weatherData.weather[0]
+//                 ? weatherData.weather[0].main
+//                 : null}
+//             </p>
+//           </div>
+
+//           <div className=" flex gap-10 items-center justify-center">
+//             <div className=" flex flex-col items-center justify-center gap-5 p-2 border-2-transparent hover:shadow-2xl hover:border-l-2 hover:border-b-2 hover:border-black rounded-md font-semibold text-lg">
+//               <img className="w-[70px]" src="../images/wind.png" alt="" />
+//               <div className="">
+//                 <p className="">{weatherData?.wind?.speed} m/sec</p>
+//                 <p>Wind speed</p>
+//               </div>
+//             </div>
+//             <div className="flex flex-col items-center justify-center gap-5 p-2 border-2-transparent hover:shadow-2xl hover:border-l-2 hover:border-b-2 hover:border-black rounded-md font-semibold text-lg">
+//               <img className="w-[70px]" src="../images/humidity.png" alt="" />{" "}
+//               <div className="">
+//                 <p>{weatherData?.main?.humidity}%</p>
+//                 <p>Humidity</p>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Weather;
 import React, { useEffect, useState } from "react";
 import Search from "./Search";
 
 const Weather = () => {
   const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(false);
-  const [errMsg, setErrMsg] = useState(false);
+  const [errMsg, setErrMsg] = useState(null);
   const [weatherData, setWeatherData] = useState("");
 
   const fetchWeatherData = async (param) => {
-    try {
-      setLoading(true);
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=5d6edc0e197ad2988ad739c90741f149
-      `
-      );
-      const data = await response.json();
-      if (data) {
-        console.log(data);
-        setWeatherData(data);
+    if (param.trim() !== "") {
+      try {
+        setLoading(true);
+        setErrMsg(null);
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=5d6edc0e197ad2988ad739c90741f149&units=metric`
+        );
+        const data = await response.json();
+        if (response.ok) {
+          setWeatherData(data);
+        } else {
+          setErrMsg(data.message);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.log("error: ", error.message);
+        setErrMsg("Failed to fetch weather data");
         setLoading(false);
       }
-    } catch (error) {
-      console.log(error);
-      setErrMsg(error.message);
-      setLoading(false);
+    } else {
+      alert("Please enter city...");
     }
   };
+
   useEffect(() => {
     fetchWeatherData("karachi");
   }, []);
+
   const handleSearch = async () => {
     fetchWeatherData(search);
   };
@@ -41,15 +176,6 @@ const Weather = () => {
     );
   }
 
-  if (errMsg) {
-    return (
-      <div className="text-xl flex items-center justify-center h-screen font-bold">
-        <h1 className="text-center">
-          Error Occured! <span className="text-red-600">{errMsg}</span>
-        </h1>
-      </div>
-    );
-  }
   const getCurrentDate = () => {
     return new Date().toLocaleDateString("en-us", {
       weekday: "long",
@@ -58,45 +184,69 @@ const Weather = () => {
       year: "numeric",
     });
   };
+
   return (
-    <div className="flex items-center   justify-center w-100 h-screen">
-      <div className="flex w-[700px] rounded-2xl bg-blue-500 m-2 mx-auto border-2 h-[670px] pt-10 flex-col items-center justify-start">
+    <div className="flex flex-col items-center p-2 justify-center w-full min-h-screen">
+      <div className="flex min-w-[90%] sm:min-w-[60%] rounded-2xl shadow-xl bg-blue-500 m-2 mx-auto border-2 min-h-[700px] pt-2 flex-col items-center justify-start">
+        <h1 className="text-center font-extrabold text-white text-3xl sm:text-4xl">
+          Weather Forecast
+        </h1>
         <Search
+          errMsg={errMsg}
           search={search}
           setSearch={setSearch}
           handleSearch={handleSearch}
         />
-        <div className="text-white">
-          <div className="">
-            <h2 className="font-bold text-3xl py-2">
-              {weatherData?.name}, {weatherData?.sys.country}
-            </h2>
+        {errMsg !== null ? (
+          <div className="text-xl flex flex-col items-center justify-center  font-bold">
+            <h1 className="text-center">
+              Error Occurred! <span className="text-red-300">{errMsg}</span>
+            </h1>
           </div>
-          <div className="">
-            <span className="font-semibold text-xl py-1">
-              {getCurrentDate()}
-            </span>
-          </div>
-          <div className="">
-            <b>Temperature: </b>
-            {weatherData?.main?.temp}
-          </div>
-          <p>
-            {weatherData && weatherData.weather[0]
-              ? weatherData.weather[0].description
-              : null}
-          </p>
-          <div className="flex gap-2 items-center justify-center">
-            <div className="">
-              <p>{weatherData?.wind?.speed}</p>
-              <p>Wind speed</p>
+        ) : (
+          <div className="text-white font-extrabold flex flex-col gap-5 text-center p-5 min-h-[500px] rounded-sm sm:w-[90%]">
+            <div className="bg-black text-white rounded-md">
+              <h2 className="font-bold text-3xl py-2">
+                {weatherData?.name}, {weatherData?.sys?.country}
+              </h2>
             </div>
-            <div className="">
-              <p>{weatherData?.main?.humidity}%</p>
-              <p>Humidity</p>
+            <div>
+              <span className="font-semibold text-xl py-1">
+                {getCurrentDate()}
+              </span>
+            </div>
+            <div className="font-semibold text-xl">
+              Temperature: {weatherData?.main?.temp}&deg;C
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <img
+                className="w-[150px] h-[150px]"
+                src={`../images/${
+                  weatherData?.weather?.[0]?.main || "default"
+                }.png`}
+                alt=""
+              />
+              <p>{weatherData?.weather?.[0]?.main}</p>
+            </div>
+
+            <div className="flex gap-10 items-center justify-center">
+              <div className="flex flex-col items-center justify-center gap-5 p-2 border-2-transparent hover:shadow-2xl hover:border-l-2 hover:border-b-2 hover:border-black rounded-md font-semibold text-lg">
+                <img className="w-[70px]" src="../images/wind.png" alt="" />
+                <div>
+                  <p>{weatherData?.wind?.speed} m/sec</p>
+                  <p>Wind speed</p>
+                </div>
+              </div>
+              <div className="flex flex-col items-center justify-center gap-5 p-2 border-2-transparent hover:shadow-2xl hover:border-l-2 hover:border-b-2 hover:border-black rounded-md font-semibold text-lg">
+                <img className="w-[70px]" src="../images/humidity.png" alt="" />
+                <div>
+                  <p>{weatherData?.main?.humidity}%</p>
+                  <p>Humidity</p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
